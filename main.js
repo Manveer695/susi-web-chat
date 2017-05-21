9ed35259e5f67f3ab7a1aea4d0dbb3abe642fcac
Manveer695 <singhmanveer1995@gmail.com>
2017-05-22 00:37:10 +0530

    web chat in react

---
+++ main.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+var axios = require('axios');
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var config = {
+   entry: './main.js',
+	
+   output: {
+      path: path.resolve(__dirname),
+      filename: 'index.js',
+   },
+	
+   devServer: {
+      inline: true,
+      port: 8080
+   },
+	
+   module: {
+      loaders: [
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+				
+            query: {
+               presets: ['es2015', 'react']
+            }
+         }
+      ]
+   }
+}
+
+module.exports = config;
